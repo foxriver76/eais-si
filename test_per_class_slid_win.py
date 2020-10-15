cbdb9e2b388d9b8b6ae34225df35924745788f80
Moritz Heusinger <moritz.heusinger@gmail.com>
2020-10-15 09:56:49 +0200

    slid window size configurable

--- test_per_class_slid_win.py
+++ test_per_class_slid_win.py
@@ -13,11 +13,12 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.decomposition import PCA
 
 ### CONFIGURATION START ###
-BATCH_SIZE:int = 10
+BATCH_SIZE:int = 50
 ITER:int = 500
 K_DIM:int = 2
 PLT_INTERACTIVE:bool = True
 DRIFT_AT:int = int(BATCH_SIZE * ITER / 2)
+SLID_WIN_SIZE:int = 200
 ### CONFIGURATION END ###
 
 # d_stream = FileStream('dataset/org_people_2.csv')
@@ -27,8 +28,12 @@ DRIFT_AT:int = int(BATCH_SIZE * ITER / 2)
 # stream = ConceptDriftStream(stream=s_stream, 
 #                             drift_stream=d_stream, position=50)
 
-stream = ConceptDriftStream(stream=SEAGenerator(classification_function=1), 
-                            drift_stream=STAGGERGenerator(classification_function=1), 
+# stream = ConceptDriftStream(stream=SEAGenerator(classification_function=1), 
+#                             drift_stream=STAGGERGenerator(classification_function=1), 
+#                             position=DRIFT_AT, width=1000)
+
+stream = ConceptDriftStream(stream=SineGenerator(classification_function=1), 
+                            drift_stream=SineGenerator(classification_function=1), 
                             position=DRIFT_AT, width=1000)
 
 pca = [IncPCA(n_components=K_DIM, forgetting_factor=1) for i in range(stream.n_classes)]
@@ -54,7 +59,7 @@ for i in range(ITER):
             
     for j in np.unique(stream.current_sample_y):
         pca[int(j)].partial_fit(stream.current_sample_x[stream.current_sample_y.ravel() == j])
-        off_pca[int(j)].fit(all_data[max(0, i-150):i*BATCH_SIZE+BATCH_SIZE][labels[max(0, i-150):i*BATCH_SIZE+BATCH_SIZE] == j])
+        off_pca[int(j)].fit(all_data[max(0, i-SLID_WIN_SIZE):i*BATCH_SIZE+BATCH_SIZE][labels[max(0, i-SLID_WIN_SIZE):i*BATCH_SIZE+BATCH_SIZE] == j])
         
     if pcs is not None:
         for j in range(stream.n_classes):
@@ -70,7 +75,16 @@ for i in range(ITER):
                 
     if off_pcs is not None:
         for j in range(stream.n_classes):
+            if pcs[j].size > 0:
+                sim = np.diag(cosine_similarity(off_pcs[j], off_pca[j].components_))
+            else:
+                sim = None
+                
             all_off_pcs[j].append(off_pca[j].components_)
+            
+            if sim is not None and sim[0] < 0.95:
+                print(f'offline drift detected {i * BATCH_SIZE}, should be {DRIFT_AT}')
+  
         
     # GET Principal Components
     pcs = [pca[int(i)].get_loadings() for i in range(stream.n_classes)]
